74ca66515e0115d1f4ed8c9e851b35f1a89e0045
Enkhtvshn <Enkhtuwshin.1201@gmail.com>
2023-01-31 18:07:00 +0800

    ddd

--- server.js
+++ server.js
@@ -6,30 +6,45 @@ const port = 8003;
 const server = express();
 server.use(cors());
 server.use(express.json());
+const { v4: uuidv4 } = require("uuid");
+const { parse } = require("path");
+const bcrypt = require("bcrypt");
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Hello express server" });
 });
 server.post("/signup", (req, res) => {
-  const { name, role } = req.body;
-  fs.readFile("users.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log("error file");
-      return;
-    }
-    console.log(data);
-    const parsedData = JSON.parse(data);
-
-    const newUser = { name, role, id: parsedData.users.length };
-    parsedData.users.push(newUser);
-    fs.writeFile("users.json", JSON.stringify(parsedData), (err) => {
-      if (err) {
-        res.status(404).json({ message: "Error" });
-      }
-      res.status(201).json({ message: "Create new user" });
-    });
-  });
+  const { name, role, email, password } = req.body;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const id = uuidv4();
+  const salted = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salted);
+  const newUser = {
+    id,
+    name,
+    role,
+    email,
+    password: hashedPassword,
+  };
+  parsedData.users.push(newUser);
+  fs.writeFileSync("users.json", JSON.stringify(parsedData));
+  res.status(201).json({ message: "amjilttai burtgegdlee" });
 });
+server.post("/signin", (req, res) => {
+  const { id, email, password } = req.body;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const findUser = parsedData.users.find((user) => user.id === id);
+  if (!findUser) {
+    res.status(401).json({ message: "Iim hereglegch oldsngui" });
+  }
+  const isCheck = bcrypt.compareSync(password, findUser.password);
+  if (isCheck) {
+    res.status(200).json({ message: "Amjlttai newterlee", user: null });
+  }
+});
+
 server.get("/users", (req, res) => {
   fs.readFile("users.json", "utf-8", (err, data) => {
     if (err) {
@@ -42,24 +57,39 @@ server.get("/users", (req, res) => {
     res.status(201).json({ users: parsedData.users });
   });
 });
-server.get("/users/:id", (req, res) => {
-  fs.readFile("users.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log("ERROR");
-      return;
-    }
-    console.log(data);
-    const parsedData = JSON.parse(data);
 
-    const { id } = req.params;
-    const user = parsedData.users.find((el) => el.id === Number(id));
-    res.status(200).json({ user });
-  });
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const user = parsedData.users.find((el) => el.id === id);
+  res.status(200).json({ user });
 });
 
-// server.get("/", (req, res) => {
-//   res.send("huselt amjlttai ilgeegdee");
-// });
+server.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const findIndex = parsedData.users.findIndex((el) => el.id === id);
+  parsedData.users[findIndex].name = name;
+  fs.writeFileSync("users.json", JSON.stringify(parsedData));
+  res
+    .status(201)
+    .json({ message: "Шинэ хэрэглэгчийн өгөгдөл амжилттай солигдлоо." });
+});
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const findIndex = parsedData.users.findIndex((el) => el.id === id);
+  parsedData.users.splice(findIndex, 1);
+  fs.writeFileSync("users.json", JSON.stringify(parsedData));
+  res
+    .status(201)
+    .json({ message: `${id} тай хэрэглэгч амжилттай устгагдлаа.` });
+});
+
 server.listen(port, () => {
   console.log(`server start ${port}`);
 });
