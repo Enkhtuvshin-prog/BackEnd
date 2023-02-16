1d5e2edd56acba5d5d17aa529bc29b084cde0279
Enkhtvshn <Enkhtuwshin.1201@gmail.com>
2023-02-16 15:57:30 +0800

    kkll

---
+++ controllers/travel.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const router = Router();
+
+const getUsers = (req, res) => {
+  try {
+    const travels = fs.readFileSync("travel.json", "utf-8");
+    const data = JSON.parse(travels);
+    res.status(200).json({ message: "success", data: data });
+    console.log("data:", data);
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+};
+// const createUser = (req, res) => {
+//   try {
+//     const travels = fs.readFileSync("travel.json", "utf-8");
+//     const data = JSON.parse(travels);
+//     const newData = { ...req.body };
+//     data.travel.push(newData);
+//     fs.writeFileSync("travel.json", JSON.stringify(data));
+//     res
+//       .status(201)
+//       .json({ message: "travel amjilttai burtgegdlee", data: newData });
+//   } catch (err) {
+//     return res.status(400).json({ message: err.message });
+//   }
+//   res.json({});
+// };
+const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const data = fs.readFileSync("travel.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const findIndex = parsedData.users.findIndex((el) => el.id === id);
+  parsedData.users.splice(findIndex, 1);
+  fs.writeFileSync("travel.json", JSON.stringify(parsedData));
+  res.status(201).json({ message: `${id} hereglegch amjilttai ustlaa ` });
+};
+module.exports = { createUser };
+module.exports = { getUsers };
+module.exports = { updateUser };
--- routes/travel.js
+++ routes/travel.js
@@ -1,30 +1,8 @@
-const { Router } = require("express");
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
+const { Router, json } = require("express");
+const { getUsers, updateUser } = require("../controllers/travel");
 const router = Router();
 
-router.post("/", (req, res)=>{
-    try{
-        const travels = fs.readFileSync("travel.json", "utf-8");
-        const data = JSON.parse(travels);
-        const newData ={...req.body};
-        data.travel.push(newData);
-        fs.writeFileSync("travel.json", JSON.stringify(data));
-        res.status(201).json({message: "travel amjilttai burtgegdlee", data: newData})
-    }catch(err){
-        return res.status(400).json({message: err.message});
-    }
-    res.json({});
-});
-router.get("/", (req, res) => {
-    try {
-      const travels = fs.readFileSync("travel.json", "utf-8");
-      const data = JSON.parse(travels);
-      res.status(200).json({ message: "success", data: data });
-      console.log("data:", data);
-    } catch (err) {
-      return res.status(400).json({ message: err.message });
-    }
-  });
+// router.post("/", getUsers);
+router.get("/", getUsers).post("/", createUser).put("/:id", updateUser);
 
 module.exports = router;
